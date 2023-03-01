cd839b881e7e072582bc3bda6d84ae708b40c7e6
williamsbobby1423 <williamsbobby1423>
2023-03-01 17:57:37 +0000

    Initial commit

---
+++ src/main/java/com/example/operational_excellence_testing/Application.java
@@ -0,0 +1,10 @@
+package com.example.operational_excellence_testing;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
